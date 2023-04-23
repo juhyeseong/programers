daf02d176f3be7b28603bb6174c1a3657e008b8a
juhyeseong <130978861+juhyeseong@users.noreply.github.com>
2023-04-23 21:56:39 +0900

    [unrated] Title: 정수 부분, Time: 0.03 ms, Memory: 72.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181850. 정수 부분/정수 부분.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(double flo) {
+        int answer = 0;
+        boolean limit = 0 <= flo && flo <= 100;
+        
+        answer = (int)flo;
+        
+        return answer;
+    }
+}
