2840fb849b4d40823b9769f2fd3c4f3f67f154c8
juhyeseong <130978861+juhyeseong@users.noreply.github.com>
2023-04-24 17:33:39 +0900

    Update 두 수의 나눗셈.java

--- 프로그래머스/lv0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
+++ 프로그래머스/lv0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
@@ -5,6 +5,9 @@ class Solution {
         
         if(limit) {
             answer = (int)((num1 / (double)num2) * 1000);
+            // 1. 먼저 num2에 double타입으로 강제 형변환을하여 num1 과 num2의 나눗셈의 실수값을 구해준다.
+            // 2. 다음은 1000을 곱해주면 실수값이 나오는데 그 값의 정수부분만 return되도록하기 위해
+            // 3. 마지막으로 전체 결과값에 int로 강제 형변환을 해주어 정수부분만 추출한다.
         }
         
         return answer;
