2306b47204128f4c9df7d32c4840faf744e2c777
juhyeseong <130978861+juhyeseong@users.noreply.github.com>
2023-04-24 17:39:36 +0900

    Update 피자 나눠 먹기 （1）.java

--- 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
+++ 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
@@ -4,12 +4,12 @@ class Solution {
         boolean limit = 1 <= n && n <= 100;
         
         if(limit) {
-            for(int i = 1; i < n; i++) {
-                if(i % 7 == 0 && i != 7) {
-                    answer++;
+            for(int i = 1; i < n; i++) {        // 사람이 1명 이상이 있어야 피자의 갯수를 셀 수 있게 1 ~ 인원수만큼 반복을 지정
+                if(i % 7 == 0 && i != 7) {      // 1판에 7조각이므로 answer에 1을 넣어줬기때문에 7명까지는 한조각이 되어야한다
+                    answer++;                   // 그러므로 i != 7이 아닐때 answer의 값에 1이 더 해지게 해준다
                 }
-                else if(i == 8) {
-                    answer++;
+                else if(i == 8) {               // 하지만 조건을 7로 나눴을 때 +1이 되게 if문을 걸었기 때문에 8명째에서는 +1이 되지 않기 때문에
+                    answer++;                   // 첫번째 피자 추가에서는 8일때 추가할 수 있게 else if문을 
                 }
             }
         }
