abfd156db707915dc076bb5e8f6e9f272c234064
juhyeseong <130978861+juhyeseong@users.noreply.github.com>
2023-04-24 18:04:17 +0900

    Update 배열 뒤집기.java

--- 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.java
+++ 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.java
@@ -5,14 +5,11 @@ class Solution {
 	        boolean limitElement = false;
 	        int cnt = 0;
 	        
-	        for(int i = num_list.length - 1; i >= 0; i--) {
-             limitElement = 0 <= num_list[i] && num_list[i] <= 1000;
-             for(int j = 0; j < answer.length; j++) {
-                 if(limitLength && limitElement) {
-                	 answer[cnt] = num_list[i];
-                     cnt += 1;
-                     break;
-                 }
+	        for(int i = num_list.length - 1; i >= 0; i--) {    	// 매개변수로 받은 배열의 길이만큼 배열의 반대순서로 반복문을 돌린다
+             limitElement = 0 <= num_list[i] && num_list[i] <= 1000; 
+             if(limitLength && limitElement) {
+                 answer[cnt] = num_list[i];    				// 매개변수로 받은 배열의 젤 뒷번째부터 순차적으로 값을 answer가 받게 된다
+			 cnt += 1;					// 이때 cnt는 answer의 배열 원소의 자리가 되는 수이다
              }
          }
 	        
