02ec809a9a25855f71441aad64a6029021eee756
juhyeseong <130978861+juhyeseong@users.noreply.github.com>
2023-04-25 17:49:50 +0900

    [unrated] Title: 두 수의 연산값 비교하기, Time: 8.71 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.java
+++ 프로그래머스/unrated/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.java
@@ -7,14 +7,14 @@ class Solution {
 	        boolean limit = 1 <= a && a < 10000 && 1 <= b && b < 10000;
 	        
 	        if(limit) {
-	            sum = Integer.parseInt( a + "" + b);
-	            mul = 2 * a * b;
+	            sum = Integer.parseInt( a + "" + b);    // a와 b를 문자열로 바꿔 서로 붙여준 뒤 int값으로 파싱하여 sum변수에 저장
+	            mul = 2 * a * b;                        // 문제에서의 조건으로 두 정수를 붙여서 쓴 값과 비교할 값 준비
 	            
-	            if(sum < mul) {
-	            	answer = mul;
+	            if(sum < mul) {                         // 만약 a와 b가 붙은 값보다 비교할 mul변수가 더 크면
+	            	answer = mul;                       // mul을 반환하고
 	            }
-	            else {
-	            	answer = sum;
+	            else {                                  // a와 b가 붙은 값이 더 크거나 두 변수의 값이 같다면
+	            	answer = sum;                       // sum을 반환한다
 	            }
 	        }
 	        
