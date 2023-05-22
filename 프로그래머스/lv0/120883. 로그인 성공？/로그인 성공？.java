5243644c0440731b220df2936e33907e83c636e2
juhyeseong <130978861+juhyeseong@users.noreply.github.com>
2023-05-22 20:51:46 +0900

    [level 0] Title: 로그인 성공?, Time: 0.02 ms, Memory: 81.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String solution(String[] id_pw, String[][] db) {
+        String answer = "fail";
+        
+        for(int i = 0; i < db.length; i++) {
+            for(int j = 0; j < db[i].length; j++) {
+                if(id_pw[0].equals(db[i][0]) && id_pw[1].equals(db[i][1])) {
+                    answer = "login";
+                }
+                if(id_pw[0].equals(db[i][0]) && !id_pw[1].equals(db[i][1])) {
+                    answer = "wrong pw";
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
