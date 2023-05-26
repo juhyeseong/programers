60ff81a56c437b5fe700801212c66037393b1ff3
juhyeseong <130978861+juhyeseong@users.noreply.github.com>
2023-05-26 19:01:17 +0900

    문자열 묶기.java

--- 프로그래머스/unrated/181855. 문자열 묶기/문자열 묶기.java
+++ 프로그래머스/unrated/181855. 문자열 묶기/문자열 묶기.java
@@ -3,9 +3,9 @@ class Solution {
    public int solution(String[] strArr) {
         int answer = 0;
         ArrayList<Integer> lengthList = new ArrayList<Integer>();	// 문자열 길이
-        ArrayList<Integer> cntList = new ArrayList<Integer>();//문자열 목록
+        ArrayList<Integer> cntList = new ArrayList<Integer>();    //문자열 목록
         
-        for(String str : strArr) {
+        for(String str : strArr) {                                // 문자열 배열 strArr의 원소들의 문자열 길이의 종류를 담는 연산
         	if(lengthList.size() == 0) {
         		lengthList.add(str.length());
         	}
@@ -24,9 +24,9 @@ class Solution {
         	}
         }
         
-        lengthList.sort(null);
+        lengthList.sort(null);                                    
         
-        for(int i = 0; i < lengthList.size(); i++) {
+        for(int i = 0; i < lengthList.size(); i++) {               // 문자열의 길이 별 갯수를 담는 연산
         	int cnt = 0;
         	
         	for(String str : strArr) {
@@ -37,9 +37,9 @@ class Solution {
         	cntList.add(cnt);
         }
         
-        cntList.sort(Comparator.reverseOrder());
-        
-        answer = cntList.get(0);
+        cntList.sort(Comparator.reverseOrder());                     // 문자열의 길이 별 갯수 내림차순
+         
+        answer = cntList.get(0);                                     // 내림차순이니 리스트의 첫 번째 값이 갯수가 가장 큰 집합
         
         return answer;
     }
