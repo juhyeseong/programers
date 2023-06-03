770d0f8f53d92a95f8bd369c33f128925ae9311e
juhyeseong <130978861+juhyeseong@users.noreply.github.com>
2023-06-03 16:38:10 +0900

    [level 0] Title: 외계행성의 나이, Time: 13.91 ms, Memory: 71.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120834. 외계행성의 나이/README.md
+++ 프로그래머스/lv0/120834. 외계행성의 나이/README.md
@@ -1,10 +1,10 @@
 # [level 0] 외계행성의 나이 - 120834 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120834?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120834) 
 
 ### 성능 요약
 
-메모리: 80.4 MB, 시간: 12.86 ms
+메모리: 71.8 MB, 시간: 13.91 ms
 
 ### 구분
 
--- 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.java
+++ 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.java
@@ -1,19 +1,12 @@
 class Solution {
     public String solution(int age) {
         String answer = "";
-        String result = "";
+        String ageStr = age + "";                                           // 나이의 앞자리수부터 차례로 비교하기 편하게 문자열로 변환한 변수
+        String alpa = "abcdefghij";                                         // 0 ~ 9까지에 대항하는 알파벳을 문자열로 만든 변수
         
-        while (age != 0) {
-        	result += (age%10) +"";
-        	age = age / 10;
+        for(int i = 0; i < ageStr.length(); i++) {
+            answer += alpa.charAt(Integer.parseInt(ageStr.charAt(i) + "")); // 나이의 현 index번째 있는 자릿수의 숫자의 index번째 alpa 원소를 answer에 추가
         }
-        char[] arr = result.toCharArray();
-        
-        for(int i=arr.length-1; i>=0;i--) {
-        	answer += (char)(arr[i] + 49);
-        }
-        
-        System.out.println(result);
         
         return answer;
     }
