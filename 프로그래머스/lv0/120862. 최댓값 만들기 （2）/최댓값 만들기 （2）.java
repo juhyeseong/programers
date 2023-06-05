5b24f94f5e5b0b76463aaa4eb726af218ed290ee
juhyeseong <130978861+juhyeseong@users.noreply.github.com>
2023-06-05 23:15:46 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.03 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        
+        for(int i = 0; i < numbers.length; i++) {
+            for(int j = 0; j < numbers.length; j++) {
+                if(i == 0) {
+                    answer = numbers[i] * numbers[j];
+                }
+                if(i != j && answer < numbers[i] * numbers[j]) {
+                    answer = numbers[i] * numbers[j];
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
