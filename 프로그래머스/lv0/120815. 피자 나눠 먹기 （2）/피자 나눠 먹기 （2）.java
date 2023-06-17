77276ec828340520f6ab433287b210c24395671e
juhyeseong <130978861+juhyeseong@users.noreply.github.com>
2023-06-17 22:50:02 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.03 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
+++ 프로그래머스/lv0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
@@ -1,21 +1,14 @@
 class Solution {
     public int solution(int n) {
-      		int answer = 0;
-		if (n == 1) {
-			answer = 1;
-		} else if (n == 2) {
-			answer = 1;
-		} else if (n == 5) {
-			answer = 5;
-		} else {
-
-			for (int i = 1; i <= n; i++) {
-				if ((i * n) % 6 == 0) {
-					answer = (i * n) / 6;
-					break;
-				}
-			}
-		}
-		return answer;
+        int answer = 0;
+        
+        for(int i = 1; i <= n; i++) {
+            if((i * 6) % n == 0) {
+                answer = i;
+                break;
+            }
+        }
+        
+        return answer;
     }
 }
