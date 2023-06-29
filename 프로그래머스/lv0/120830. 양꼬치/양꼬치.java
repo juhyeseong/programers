43b1e0cab6715b6e28f33504200727fcba22cd72
juhyeseong <130978861+juhyeseong@users.noreply.github.com>
2023-06-29 22:24:42 +0900

    [level 0] Title: 양꼬치, Time: 0.04 ms, Memory: 70.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120830. 양꼬치/양꼬치.java
+++ 프로그래머스/lv0/120830. 양꼬치/양꼬치.java
@@ -1,8 +1,6 @@
 class Solution {
     public int solution(int n, int k) {
         int answer = 0;
-        boolean limitN = 0 < n && n < 1000;
-        boolean limitK = n/10 <= k && k < 1000;
         
         for(int i = 1; i <= n; i++) {
             if(i % 10 == 0) {
