3c272afba36f420b802e58781e3296f6d7185eeb
juhyeseong <130978861+juhyeseong@users.noreply.github.com>
2023-06-30 21:42:12 +0900

    [unrated] Title: n 번째 원소까지, Time: 0.03 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181889. n 번째 원소까지/n 번째 원소까지.java
+++ 프로그래머스/unrated/181889. n 번째 원소까지/n 번째 원소까지.java
@@ -1,19 +1,15 @@
 class Solution {
     public int[] solution(int[] num_list, int n) {
 	        int[] answer = new int[n];
-	        boolean limitLength = 2 <= num_list.length && num_list.length <= 30 && 1 <= n && n <= num_list.length;
-	        boolean limitOb = false;
-	        
-	        if(limitLength) {
-	            for(int i = 0; i < n; i++) {
-	                for(int j = 0; j < answer.length; j++) {
-	                    if(answer[j] == 0 && 1 <= num_list[i] && num_list[i] <= 9) {
-	                        answer[j] = num_list[i];
-                            break;
-	                    }
-	                }
-	            } 
-	        }
+        
+            for(int i = 0; i < n; i++) {
+                for(int j = 0; j < answer.length; j++) {
+                    if(answer[j] == 0 && 1 <= num_list[i] && num_list[i] <= 9) {
+                        answer[j] = num_list[i];
+                        break;
+                    }
+                }
+            } 
 	        
 	        return answer;
 	    }
