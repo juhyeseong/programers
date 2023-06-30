7156bcbfc597468679a26e85fbc07175dea0b697
juhyeseong <130978861+juhyeseong@users.noreply.github.com>
2023-06-30 21:44:47 +0900

    [unrated] Title: 부분 문자열, Time: 0.03 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181842. 부분 문자열/부분 문자열.java
+++ 프로그래머스/unrated/181842. 부분 문자열/부분 문자열.java
@@ -2,8 +2,8 @@ class Solution {
     public int solution(String str1, String str2) {
         int answer = 0;
         
-        if(str2.contains(str1)) {
-            answer = 1;
+        if(str2.contains(str1)) {   // 만약 str2에 str1이 포함되어있으면
+            answer = 1;             // 1을 반환
         }
         
         return answer;
