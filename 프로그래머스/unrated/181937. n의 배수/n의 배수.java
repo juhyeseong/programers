50a421214e1ba051885f61b7abc6ea6754797044
juhyeseong <130978861+juhyeseong@users.noreply.github.com>
2023-07-05 19:31:59 +0900

    [unrated] Title: n의 배수, Time: 0.01 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181937. n의 배수/n의 배수.java
+++ 프로그래머스/unrated/181937. n의 배수/n의 배수.java
@@ -2,7 +2,7 @@ class Solution {
     public int solution(int num, int n) {
         int answer = 0;
         
-        answer = num % n == 0 ? 1 : 0;
+        answer = num % n == 0 ? 1 : 0;  // num가 n의 배수이면 1을 아니면 0을 반환
         
         return answer;
     }
