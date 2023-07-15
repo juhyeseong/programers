7068c807986c7feeb3175f9d2c67ec18e53d461d
juhyeseong <130978861+juhyeseong@users.noreply.github.com>
2023-07-15 20:52:22 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 8.93 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
+++ 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
@@ -3,14 +3,14 @@ class Solution {
         String answer = "";
         int index = 0;
         
-        if(s.length() % 2 == 0) {
-            index = (s.length() / 2) - 1;
-            answer += s.charAt(index);
-            answer += s.charAt(index + 1);
+        if(s.length() % 2 == 0) {           // 문자열 s의 길이가 짝수이면
+            index = (s.length() / 2) - 1;   // 문자열 s의 길이의 절반값에 1을 뺀 값을 index값으로 정하고
+            answer += s.charAt(index);      // 문자열 s의 지정한 index번째 원소를 answer에 담고
+            answer += s.charAt(index + 1);  // 그 다음값까지 answer에 담는다
         }
-        else {
-            index = s.length() / 2;
-            answer += s.charAt(index);
+        else {                              // 만약 s의 길이가 홀수이면
+            index = s.length() / 2;         // 문자열 s의 길이의 절반값에 1을 뺀 값을 index값으로 정하고
+            answer += s.charAt(index);      // 문자열 s의 지정한 index번째 원소만 answer에 담는다
         }
         
         return answer;
