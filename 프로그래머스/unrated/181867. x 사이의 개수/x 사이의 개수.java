a2c4a43f01dbebe0d1cfd20c0f633de7dab85ae3
juhyeseong <130978861+juhyeseong@users.noreply.github.com>
2023-08-30 19:26:35 +0900

    [unrated] Title: x 사이의 개수, Time: 5.05 ms, Memory: 80.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181867. x 사이의 개수/x 사이의 개수.java
+++ 프로그래머스/unrated/181867. x 사이의 개수/x 사이의 개수.java
@@ -1,24 +1,15 @@
+import java.util.*;
+
 class Solution {
-    public int[] solution(String myString) {
-      int[] answer = {};
+    public ArrayList<Integer> solution(String myString) {
+        ArrayList<Integer> answer = new ArrayList<Integer>();
+        String[] arr = myString.split("x");                     // x를 구분자로 String배열에 각각 값을 넣어준다
         
-        if(myString.endsWith("x")) {
-        	myString += " ";
+        for(int i = 0; i < arr.length; i++) {           
+            answer.add(arr[i].length());                        // 배열에 담긴 각각의 문자열 길이를 answer에 추가
         }
-
-        
-        String[] arr = myString.split("x");
-        
-
-        
-        for(int i=0; i<arr.length;i++) {
-        	arr[i] = arr[i].replace(" ", "");
-        }
-        
-        answer = new int[arr.length];
-        
-        for(int i=0; i<answer.length;i++) {
-        	answer[i] = arr[i].length();
+        if(myString.charAt(myString.length() - 1) == 'x') {     // myString의 마지막 index의 원소가 x이면
+            answer.add(0);                                      // answer의 마지막에 0을 추가
         }
         
         return answer;
