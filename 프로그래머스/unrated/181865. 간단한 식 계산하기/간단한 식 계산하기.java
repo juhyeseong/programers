db0be8d4cda98d0941dca416672fa3ea5a29949c
juhyeseong <130978861+juhyeseong@users.noreply.github.com>
2023-09-18 15:55:26 +0900

    [unrated] Title: 간단한 식 계산하기, Time: 0.18 ms, Memory: 66.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181865. 간단한 식 계산하기/간단한 식 계산하기.java
+++ 프로그래머스/unrated/181865. 간단한 식 계산하기/간단한 식 계산하기.java
@@ -1,35 +1,23 @@
+import java.util.*;
+
 class Solution {
     public int solution(String binomial) {
-       int answer = 0;
-        String left = "";
-        String right = "";
+        int answer = 0;
+        ArrayList<String> list = new ArrayList<String>(Arrays.asList(binomial.split(" ")));
         
-        if (binomial.contains("*")) {
-        	left = binomial.substring(0, binomial.indexOf("*"));
-        	right = binomial.substring(binomial.indexOf("*")+1, binomial.length());
-        	
-        	int left2 = Integer.parseInt(left.replace(" ", ""));
-        	int right2 = Integer.parseInt(right.replace(" ", ""));
-        	
-        	answer = left2 * right2;
-       
-        } else if (binomial.contains("+")) {
-        	left = binomial.substring(0, binomial.indexOf("+"));
-        	right = binomial.substring(binomial.indexOf("+")+1, binomial.length());
-        	
-        	int left2 = Integer.parseInt(left.replace(" ", ""));
-        	int right2 = Integer.parseInt(right.replace(" ", ""));
-        	
-        	answer = left2 + right2;
-        } else if (binomial.contains("-")) {
-          	left = binomial.substring(0, binomial.indexOf("-"));
-        	right = binomial.substring(binomial.indexOf("-")+1, binomial.length());
-        	
-        	int left2 = Integer.parseInt(left.replace(" ", ""));
-        	int right2 = Integer.parseInt(right.replace(" ", ""));
-        	
-        	answer = left2 - right2;
+        if(list.get(1).equals("+")) {
+            answer = Integer.parseInt(list.get(0)) + Integer.parseInt(list.get(2));
         }
+        else if(list.get(1).equals("-")) {
+            answer = Integer.parseInt(list.get(0)) - Integer.parseInt(list.get(2));
+        }
+        else if(list.get(1).equals("*")) {
+            answer = Integer.parseInt(list.get(0)) * Integer.parseInt(list.get(2));
+        }
+        else {
+            answer = Integer.parseInt(list.get(0)) / Integer.parseInt(list.get(2));
+        }
+        
         return answer;
     }
 }
