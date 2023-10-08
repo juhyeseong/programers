99ec312e1302462f375931f146d7974cb0b59bbb
juhyeseong <130978861+juhyeseong@users.noreply.github.com>
2023-10-08 16:08:25 +0900

    [level 1] Title: 소수 찾기, Time: 185.95 ms, Memory: 52.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.java
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.java
@@ -1,10 +1,11 @@
 class Solution {
     public int solution(int n) {
-        int answer = 1;                     // 2를 제외한 짝수는 소수가 아니기 때문에 3부터 시작해서 짝수인 것은 제외(3부터 시작하지만 2는 소수이기 때문에 answer의 갯수는 1부터 시작)
+        int answer = 1;                                     // 2를 제외한 짝수는 소수가 아니기 때문에 3부터 시작해서 짝수인 것은 제외(3부터 시작하지만 2는 소수이기 때문에 answer의 갯수는 1부터 시작)
         
         for(int i = 3; i <= n; i++) {
-            boolean decimal = true;             // 소수인지 판별하는 boolean변수
+            boolean decimal = true;                         // 소수인지 판별하는 boolean변수
             if(i % 2 != 0) {
+                                                            // i가 소수인지 판별하기 위해서는 2 ~ 자기자신의 제곱근까지만 판별해도 소수인지 판별이 가능하다
                 for(int j = 2; j <= Math.sqrt(i); j++) {    // 2 ~ 자기자신의 제곱근까지
                     if(i % j == 0) {                        // 자신과 나눠서 0으로 떨어지는게 있다면
                         decimal = false;                    // 소수가 아니다
